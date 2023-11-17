70ab51c941f447201ccdb9d8eba9129792695591
asanmor916 <asanmor916@iesalmudeyne>
2023-11-17 10:30:34 +0100

    Saludo con colores

--- holaMundo.java
+++ holaMundo.java
@@ -2,7 +2,7 @@ public class holaMundo {
     public static void main (String[]args){
         String rojo = "\033[32m";
         String azul = "\033[34m";
-        System.out.println("HOLA MUNDO");
-        System.out.println("ADIOS MUNDO");
+        System.out.println(rojo +"HOLA MUNDO");
+        System.out.println(azul +"ADIOS MUNDO");
     }
 }
