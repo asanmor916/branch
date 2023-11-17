23d0c3a48fd9995b17a3ce64fd1b3e2e0c74b73a
asanmor916 <asanmor916@iesalmudeyne>
2023-11-17 10:25:24 +0100

    Version del fichero hola mundo con colores

--- holaMundo.java
+++ holaMundo.java
@@ -1,5 +1,7 @@
 public class holaMundo {
     public static void main (String[]args){
+        String rojo = "\033[32m";
+        String azul = "\033[34m";
         System.out.println("HOLA MUNDO");
         System.out.println("ADIOS MUNDO");
     }
